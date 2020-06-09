6f1422fec1ec5afb78b155aa757b548655b67b72
brunoluciano <bruno.lucian0544@gmail.com>
2020-06-08 23:50:19 -0300

    Adicionado migration da tabela de produtos e relacionados

---
+++ database/migrations/2020_06_09_022512_create_produtos_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProdutosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produtos', function (Blueprint $table) {
+            $table->id();
+            $table->string('descricao');
+
+            $table->unsignedBigInteger('categoria_id');
+            $table->unsignedBigInteger('marca_id');
+            $table->unsignedBigInteger('tipo_id');
+            $table->unsignedBigInteger('unidade_medida_id');
+            $table->double('unidade');
+            $table->string('cod_barras');
+            $table->bigInteger('estoque_minimo');
+            $table->bigInteger('estoque_maximo');
+            $table->double('preco_compra');
+            $table->double('preco_venda');
+            $table->string('path_img');
+
+            $table->foreign('categoria_id')->references('id')->on('categorias');
+            $table->foreign('marca_id')->references('id')->on('marcas');
+            $table->foreign('tipo_id')->references('id')->on('tipoprodutos');
+            $table->foreign('unidade_medida_id')->references('id')->on('unidademedidas');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produtos');
+    }
+}
